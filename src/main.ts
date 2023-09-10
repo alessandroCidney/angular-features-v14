3306db2c6d9ad4ef27323aec32ff09a950153894
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-09-10 16:18:40 -0300

    Standalone components class

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { AppComponent } from './app/app.component'
+import { importProvidersFrom } from '@angular/core'
+import { AppRoutingModule } from './app/app-routing.module'
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+  ]
+})
   .catch(err => console.error(err));
